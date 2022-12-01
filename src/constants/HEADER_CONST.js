3b36726d676151b68f96b5df9458b48658e7feaa
leodsan <helloleonardosantiago@gmail.com>
2022-12-01 18:17:57 +0330

    add header constants

---
+++ src/constants/HEADER_CONST.js
@@ -0,0 +1,30 @@
+const HEADER_CONST = {
+  routes: [
+    {
+      id: 0,
+      title: "works",
+    },
+    {
+      id: 1,
+      title: "contact",
+    },
+    {
+      id: 2,
+      title: "about",
+    },
+  ],
+  socialMedias: [
+    {
+      id: 0,
+      icon: "github",
+      link: "/",
+    },
+    {
+      id: 1,
+      icon: "linkedin",
+      link: "/",
+    },
+  ],
+};
+
+export { HEADER_CONST };
