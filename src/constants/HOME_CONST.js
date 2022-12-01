229d138d17ab4157faa8fc46e2877b886c3cb2e7
leodsan <helloleonardosantiago@gmail.com>
2022-12-01 23:02:42 +0330

    add skills

--- src/constants/HOME_CONST.js
+++ src/constants/HOME_CONST.js
@@ -21,6 +21,80 @@ const HOME_CONST = {
     yearsOfExperiance: "+10",
     projecsDone: "130",
   },
+  skills: [
+    {
+      id: 1,
+      title: "C#",
+    },
+    {
+      id: 2,
+      title: "JavaScript",
+    },
+    {
+      id: 3,
+      title: "MySQL",
+    },
+    {
+      id: 4,
+      title: "Amazon S3",
+    },
+    {
+      id: 5,
+      title: "CloudWatch",
+    },
+    {
+      id: 6,
+      title: "NET",
+    },
+    {
+      id: 7,
+      title: "TypeScript",
+    },
+    {
+      id: 8,
+      title: "MySSQL",
+    },
+    {
+      id: 9,
+      title: "Amazon Athena",
+    },
+    {
+      id: 10,
+      title: "Lambda",
+    },
+    {
+      id: 11,
+      title: "ASP.NET",
+    },
+    {
+      id: 12,
+      title: "Angular",
+    },
+    {
+      id: 13,
+      title: "Selenium",
+    },
+    {
+      id: 14,
+      title: "RDS",
+    },
+    {
+      id: 15,
+      title: "NUnit",
+    },
+    {
+      id: 16,
+      title: "Kendo UI",
+    },
+    {
+      id: 17,
+      title: "Cypress",
+    },
+    {
+      id: 18,
+      title: "CloudFormation",
+    },
+  ],
 };
 
 export { HOME_CONST };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,26 @@
 import { Introduction } from "../components/pages/home";
+import { InfoSection } from "../components/elements";
+import { HOME_CONST } from "../constants";
 
 const Home = () => {
+  const { skills } = HOME_CONST;
+
   return (
     <main>
       <Introduction />
+      <InfoSection
+        patternP="3"
+        title="skills"
+        listPosition="left"
+        patternPClassName="top-0 right-0"
+        itemsClassName=""
+      >
+        {skills.map(({ id, title }) => (
+          <li key={id} className="py-2 tp-h3 w-[17.7%] list-disc">
+            {title}
+          </li>
+        ))}
+      </InfoSection>
     </main>
   );
 };
