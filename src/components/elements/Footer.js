b778ded7d9504f3d8235cf518d9757a6c2578c43
leodsan <helloleonardosantiago@gmail.com>
2022-12-02 19:57:07 +0330

    fix responsive footer

--- src/components/elements/Footer.js
+++ src/components/elements/Footer.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 import { GLOBAL_CONST } from "../../constants";
 import { Button } from "../commons";
 import { SocialMedias } from "./";
@@ -18,7 +20,10 @@ const Footer = () => {
         className="absolute -bottom-20 -left-20 -scale-[1] -z-10"
       />
       <div className="app-container flex flex-col items-center justify-center">
-        <div className="flex items-center justify-center w-[460px] aspect-square max-w-[65vw] relative before:-z-10 before:rotate-45 before:w-full before:h-full before:bg-green-650 before:absolute before:content-['']">
+        <Link
+          href={`mailto:${email}`}
+          className="flex items-center justify-center w-[460px] aspect-square max-w-[65vw] relative before:-z-10 before:rotate-45 before:w-full before:h-full before:bg-green-650 before:absolute before:content-['']"
+        >
           <span className="tp-display2 md:text-[14vw] pl-14 md:pl-8 whitespace-pre-line">
             {`CALL 
             Leonardo`}
@@ -27,15 +32,16 @@ const Footer = () => {
             {`let’s talk about 
             your project`}
           </span>
-        </div>
-        <div className="flex w-full items-center justify-between tp-h5 mt-60 ">
+        </Link>
+        <div className="flex w-full items-center justify-between tp-h5 mt-60 gap-5 md:flex-wrap">
           <span>all rights reserved</span>
           <Button
             target
             type="a"
             title={email}
             href={`mailto:${email}`}
-            titleClassName="tp-h5"
+            titleClassName="tp-h5 xs:tp-h6"
+            className="md:w-full order-3"
           />
           <SocialMedias />
         </div>
