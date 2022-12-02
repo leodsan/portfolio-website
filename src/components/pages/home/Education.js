6b9b1bc389f1a19ce92985828299ed2f90cc9d81
leodsan <helloleonardosantiago@gmail.com>
2022-12-02 20:04:51 +0330

    fix responsive

--- src/components/pages/home/Education.js
+++ src/components/pages/home/Education.js
@@ -1,4 +1,3 @@
-import { Button } from "../../commons";
 import { InfoSection } from "../../elements";
 
 function Education({ items }) {
@@ -7,23 +6,23 @@ function Education({ items }) {
       hasShadow
       title="education"
       listPosition="right"
-      listClassName="pt-14"
+      listClassName="pt-14 xs:pt-7"
     >
-      {items.map(({ id, title, link, date, subtitle }) => (
+      {items.map(({ id, title, date, subtitle }) => (
         <li
           key={id}
           className="list-disc w-full relative [&:not(:last-child)]:mb-14 [&:not(:last-child)]:before:content-[''] [&:not(:last-child)]:before:w-[90%] [&:not(:last-child)]:before:h-[1px] [&:not(:last-child)]:before:bg-green-500 [&:not(:last-child)]:before:left-1/2 [&:not(:last-child)]:before:-translate-x-1/2 [&:not(:last-child)]:before:bottom-0 [&:not(:last-child)]:before:absolute"
         >
-          <Button target type="a" href={link} className="w-full pb-14 gap-9">
-            <div className="flex items-center justify-between flex-grow tp-h1 md:flex-col md:items-start gap-3">
+          <div className="flex items-center w-full pb-14 xs:pb-7 gap-9">
+            <div className="flex items-center justify-between flex-grow tp-h1 sm:tp-h3 xs:tp-h4 md:flex-col md:items-start gap-3">
               <div className="flex gap-5 items-end lg:flex-col lg:items-start lg:gap-2">
                 <span>{title}</span>
                 <span className="lg:hidden">_</span>
-                <span className="tp-h4">{subtitle}</span>
+                <span className="tp-h4 xs:tp-h5">{subtitle}</span>
               </div>
-              <span className="md:!tp-h4">{date}</span>
+              <span className="md:!tp-h4 xs:tp-h5">{date}</span>
             </div>
-          </Button>
+          </div>
         </li>
       ))}
     </InfoSection>
--- src/components/pages/home/Experience.js
+++ src/components/pages/home/Experience.js
@@ -11,18 +11,20 @@ function Experience({ items }) {
       patternPClassName="top-[9.2rem] right-0"
       patternNClassName="-bottom-[16rem] left-0 z-10"
     >
-      {items.map(({ id, title, link, date, subtitle }, index) => (
+      {items.map(({ id, title, date, subtitle }, index) => (
         <li
           key={id}
           className="w-full relative [&:not(:last-child)]:mb-14 md:[&:not(:last-child)]:mb-10 [&:not(:last-child)]:before:content-[''] [&:not(:last-child)]:before:w-[90%] xs:[&:not(:last-child)]:before:w-[100%] [&:not(:last-child)]:before:h-[1px] [&:not(:last-child)]:before:bg-green-500 [&:not(:last-child)]:before:left-1/2 [&:not(:last-child)]:before:-translate-x-1/2 [&:not(:last-child)]:before:bottom-0 [&:not(:last-child)]:before:absolute"
         >
-          <div className="flex w-full pb-14 gap-9 md:gap-6 md:pb-10 xs:flex-col xs:gap-4">
-            <span className="text-9xl lg:text-8xl xs:text-7xl text-text-3">
+          <div className="flex items-center w-full pb-14 gap-9 md:gap-6 md:pb-10 xs:flex-col xs:gap-4">
+            <span className="text-9xl lg:text-8xl xs:text-6xl text-text-3">
               {("0" + (index + 1)).slice(-2)}
             </span>
-            <div className="flex items-center justify-between gap-x-6  gap-y-2 flex-grow tp-h4 md:tp-h5 2md:flex-wrap">
-              <div className="flex flex-col gap-2 items-start 2md:w-full">
-                <span className="tp-h1 lg:tp-h2 md:tp-h4">{title}</span>
+            <div className="flex items-center justify-between gap-x-6  gap-y-2 flex-grow tp-h4 md:tp-h5 xs:tp-h6 2md:flex-wrap xs:flex-col xs:text-center xs:justify-center">
+              <div className="flex flex-col gap-2 items-start 2md:w-full xs:items-center">
+                <span className="tp-h1 lg:tp-h2 md:tp-h4 xs:tp-h5">
+                  {title}
+                </span>
                 <span>{subtitle}</span>
               </div>
               <span>{date}</span>
