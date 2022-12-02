c55b12537736f03d046b5f957776750fedca7f60
leodsan <helloleonardosantiago@gmail.com>
2022-12-02 15:17:00 +0330

    create a responsive header

--- src/components/elements/Header.js
+++ src/components/elements/Header.js
@@ -6,8 +6,9 @@ const Header = () => {
   const { routes } = HEADER_CONST;
 
   return (
-    <header className="app-container z-50 flex items-center justify-center py-8">
-      <nav className="flex items-center gap-10 mr-auto">
+    <header className="app-container z-50 flex items-center justify-center py-8 sm:py-5">
+      <Button icon="menu" className="hidden mr-auto lg:!flex" />
+      <nav className="flex items-center gap-x-10 gap-y-2 mr-auto lg:flex-col lg:hidden lg:overflow-hidden lg:items-start lg:absolute lg:py-7 lg:pr-5 lg:left-[var(--app-space)] lg:top-[50%]">
         {routes.map(({ id, title }) => (
           <Button
             key={id}
@@ -23,7 +24,7 @@ const Header = () => {
         type="link"
         className="w-min absolute"
         title="leonardo santiago"
-        titleClassName="tp-h2 uppercase text-center"
+        titleClassName="tp-h2 !leading-5 uppercase text-center sm:tp-h3 xs:tp-h5 "
       />
       <SocialMedias />
     </header>
--- src/components/elements/SocialMedias.js
+++ src/components/elements/SocialMedias.js
@@ -1,11 +1,12 @@
+import cn from "classnames";
 import { GLOBAL_CONST } from "../../constants";
 import { Button } from "../commons";
 
-const SocialMedias = () => {
+const SocialMedias = ({ className }) => {
   const { socialMedias } = GLOBAL_CONST;
 
   return (
-    <div className="flex items-center gap-9">
+    <div className={cn("flex items-center gap-9 sm:gap-4", className)}>
       {socialMedias.map(({ id, icon, link }) => (
         <Button key={id} target type="a" icon={icon} href={link} />
       ))}
