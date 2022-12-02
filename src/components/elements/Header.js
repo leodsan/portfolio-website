c133e03fffb4004a506a08da88b91406755cab6b
leodsan <helloleonardosantiago@gmail.com>
2022-12-02 20:23:09 +0330

    add animation to header

--- src/components/elements/Header.js
+++ src/components/elements/Header.js
@@ -1,12 +1,36 @@
+import gsap from "gsap";
+
 import { Button } from "../commons";
 import { HEADER_CONST } from "../../constants";
 import { SocialMedias } from "./SocialMedias";
+import { useEffect } from "react";
 
 const Header = () => {
   const { routes } = HEADER_CONST;
 
+  useEffect(() => {
+    gsap.set(["#header > *"], {
+      yPercent: -150,
+      opacity: 0,
+    });
+
+    gsap.to(["#header > *"], {
+      yPercent: 0,
+      duration: 1,
+      opacity: 1,
+      ease: "power3.inOut",
+      stagger: {
+        each: 0.1,
+        from: "center",
+      },
+    });
+  }, []);
+
   return (
-    <header className="app-container z-50 flex items-center justify-center py-8 sm:py-5">
+    <header
+      id="header"
+      className="app-container z-50 flex items-center justify-center py-4 my-4 sm:py-5 overflow-hidden"
+    >
       <nav className="flex items-center gap-x-10 gap-y-2 mr-auto">
         {routes.map(({ id, title }) => (
           <Button
@@ -18,13 +42,10 @@ const Header = () => {
           />
         ))}
       </nav>
-      <Button
-        href="/"
-        type="link"
-        className="w-min absolute"
-        title="leonardo santiago"
-        titleClassName="tp-h2 !leading-5 uppercase text-center sm:tp-h3 xs:tp-h5 "
-      />
+      <span className="tp-h2 !leading-5 whitespace-pre-line uppercase text-center sm:tp-h3 xs:tp-h5 absolute">
+        {`leonardo 
+        santiago`}
+      </span>
       <SocialMedias />
     </header>
   );
